f73c0996032476f94e27862f2d9839204faf874e
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-11-04 23:45:34 -0500

    update php

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,3 +1,3 @@
 <div class="jumbotron">
-	<h1>Hello there!</h1>
+	<h1>Hello User</h1>
 </div>
--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -11,7 +11,7 @@
 		margin-top:20px;
 	}
 	.footer {
-		font-size: 14px;
+		font-size: 15px;
 		text-align: center;
 	}
 </style>
