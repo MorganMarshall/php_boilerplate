890b33c8bd8bdeec6b05c480b57ee27a73678176
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-11-04 23:30:15 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php include "includes/a_config.php";?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "includes/head-tag-contents.php";?>
+</head>
+<body>
+
+<?php include "includes/design-top.php";?>
+<?php include "includes/navigation.php";?>
+
+<div class="container" id="main-content">
+	<h2>Welcome to my website!</h2>
+	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+
+</div>
+
+<?php include "includes/footer.php";?>
+<div   style="text-align: center;">
+<?php
+
+$myname = "morgan";
+
+echo '<div  style="text-align: center; padding-top: 60px;">
+
+<div style="width: 18rem; box-shadow: 0 6px 6px -6px #777;" class="card mx-auto">
+
+<div class="card-body">
+<h3>SignUp </h3>
+</div>
+
+</div>
+
+
+'
+
+;
+$myname
+?>
+
+<?php
+echo 2 + 2;
+?>
+
+
+</div>
+
+<h1>hey yall</h1>
+</body>
+</html>
